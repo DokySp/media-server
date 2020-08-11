db5f31316ceabdf309e6b95992ad9dc145648832
DokySp <uhug@naver.com>
2020-08-11 20:01:12 +0900

    사용자 IP 가져오기+port #2

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,9 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log("/user req info");
-  console.dir(res.connection.remoteAddress);
+  console.dir(req.connection.remoteAddress);
   console.dir(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+  console.dir(req.socket._peername);
   res.send('respond with a resource');
 });
 
