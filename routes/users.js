1d1f84e48260cf1a122ba9cd2f07db7fee887498
DokySp <uhug@naver.com>
2020-08-12 11:28:07 +0900

    POST 데이터 수신 #1

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,12 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log("/user req info");
-  console.dir(req.connection.remoteAddress);
-  console.dir(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
-  console.dir(req.socket._peername);
+  // console.dir(req.connection.remoteAddress);
+  // console.dir(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+  // console.dir(req.socket._peername);
+  console.dir(req);
   res.send('Peer DATA=/IP:' + req.socket._peername.address + "/PORT:" + req.socket._peername.port + "/FAM:" +req.socket._peername.family);
+  
 });
 
 module.exports = router;
