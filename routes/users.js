2bdc312ccfca537912c837e543afd07142593123
DokySp <uhug@naver.com>
2020-08-18 18:02:15 +0900

    이미지 손상 방지 코드 추가 #4; Mutex Lock 코드 추가

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 const { tmpdir } = require('os');
+var incomming = false; // Mutex Locker
 
 /* GET users listing. */
 router.get('/:num', function(req, res, next) {
@@ -12,29 +13,29 @@ router.get('/:num', function(req, res, next) {
   // console.dir(req.socket._peername);
   // console.dir(req);
   // res.send('Peer DATA=/IP:' + req.socket._peername.address + "/PORT:" + req.socket._peername.port + "/FAM:" +req.socket._peername.family);
+  if(incomming == true) res.send('<img src="/stream/'+imgSrc+'"><SCRIPT language="JavaScript">setTimeout("history.go(0);", 40);</SCRIPT>');
+  else res.send('<h1>Loading...</h1>')
   
-  res.send('<img src="/stream/'+imgSrc+'"><SCRIPT language="JavaScript">setTimeout("history.go(0);", 40);</SCRIPT>');
 });
 
 router.post('/:num', function(req, res) {
-  var imgTmpSrc = "img"+req.params.num+"-tmp.jpg"
-  var imgSrc = "img"+req.params.num+".jpg"
+  var imgTmpSrc = './public/stream/'+"img"+req.params.num+"-tmp.jpg"
+  var imgSrc = './public/stream/'+"img"+req.params.num+".jpg"
   var imagedata = ''
   req.setEncoding('binary')
   var tt = 0
   
   // console.log(req.headers)
+  incomming = true;
   req.on('data', (chunk) => {
     imagedata += chunk
   });
 
   req.on('end', function(){
-    fs.writeFile('./public/stream/'+imgTmpSrc, imagedata, 'binary', function(err){
+    fs.writeFile(imgTmpSrc, imagedata, 'binary', function(err){
       if (err) throw err
-      fs.rename('./public/stream/'+imgTmpSrc, './public/stream/'+imgSrc, function(err) {
-        if (err) throw err
-        // console.log('File saved.')
-      })
+      // console.log('File saved.')
+      incomming = false;
     })
   });
   res.send("{'result':'ok'}");
