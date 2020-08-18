ec0a9f448db218ebd3cfc1e080c1fdc5440e3fd8
DokySp <uhug@naver.com>
2020-08-18 18:28:22 +0900

    새로고침 속도 변경

--- routes/users.js
+++ routes/users.js
@@ -12,9 +12,8 @@ router.get('/:num', function(req, res, next) {
   // console.dir(req.socket._peername);
   // console.dir(req);
   // res.send('Peer DATA=/IP:' + req.socket._peername.address + "/PORT:" + req.socket._peername.port + "/FAM:" +req.socket._peername.family);
-  res.send('<img height="100%" src="/stream/'+imgSrc+'"><SCRIPT language="JavaScript">setTimeout("history.go(0);", 1);</SCRIPT>');
+  res.send('<img height="100%" src="/stream/'+imgSrc+'"><SCRIPT language="JavaScript">setTimeout("history.go(0);", 10);</SCRIPT>');
 
-  
 });
 
 router.post('/:num', function(req, res) {
